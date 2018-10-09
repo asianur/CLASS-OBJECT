4b7e1ee58dcb6513270b7cb03747a92cd6bac146
asianur <43776916+asianur@users.noreply.github.com>
2018-10-09 23:43:05 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,27 @@
+
+public class Node {
+		//Attributes
+		String Label;
+		int Value;
+		
+		//ini method getter
+		public String getLabel()
+		{
+			return Label;
+		}
+		
+		public int getValue()
+		{
+			return Value;
+		}
+		
+		//ini method setter
+		public void setLabel(String Label)
+		{
+			this.Label = Label;
+		}
+		public void setValue(int Value)
+		{
+			this.Value = Value;
+		}
+}
